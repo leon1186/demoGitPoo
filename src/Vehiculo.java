bb89f5ad82d2458de84703b6bd6a050ad070d022
francis leon <francisleon1186@yahoo.com>
2024-10-31 08:22:09 -0500

    se crea un vehiculo

---
+++ src/Vehiculo.java
@@ -0,0 +1,12 @@
+public class Vehiculo {
+
+    String placa;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
